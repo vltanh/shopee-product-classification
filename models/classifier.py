ec724a3c1e21fc9387f064edc8d69fe56229e047
vltanh <vltheanh030698@gmail.com>
2020-06-25 22:48:07 +0700

    add classifier with attention

--- models/classifier.py
+++ models/classifier.py
@@ -8,7 +8,7 @@ from .modules.aspp import ASPP
 
 
 class BaselineWithLinear(nn.Module):
-    def __init__(self, extractor_cfg, hidden_dim, nclasses):
+    def __init__(self, extractor_cfg, nclasses, hidden_dim):
         super().__init__()
         self.nclasses = nclasses
         self.extractor = getter.get_instance(extractor_cfg)
@@ -26,7 +26,7 @@ class BaselineWithLinear(nn.Module):
 
 
 class BaselineWithASPP(nn.Module):
-    def __init__(self, extractor_cfg, aspp_hidden_dim, aspp_output_dim, nclasses):
+    def __init__(self, extractor_cfg, nclasses, aspp_hidden_dim, aspp_output_dim):
         super().__init__()
         self.nclasses = nclasses
         self.extractor = getter.get_instance(extractor_cfg)
@@ -41,5 +41,25 @@ class BaselineWithASPP(nn.Module):
     def forward(self, x):
         x = self.extractor.get_feature_map(x)
         x = self.aspp(x)
-        x = F.adaptive_avg_pool2d(x, (1, 1)).view(-1, self.aspp_output_dim)
+        x = F.adaptive_avg_pool2d(x, 1).view(-1, self.aspp_output_dim)
+        return self.classifier(x)
+
+
+class BaselineWithAttention(nn.Module):
+    def __init__(self, extractor_cfg, nclasses, nheads):
+        super().__init__()
+        self.nclasses = nclasses
+        self.extractor = getter.get_instance(extractor_cfg)
+        self.feature_dim = self.extractor.feature_dim
+        self.attn = nn.MultiheadAttention(self.feature_dim, nheads)
+        self.classifier = nn.Linear(self.feature_dim, self.nclasses)
+
+    def forward(self, x):
+        x = self.extractor.get_feature_map(x)
+        B, D, H, W = x.shape
+        x = x.reshape(B, D, -1)  # x: B, D, HW
+        x = x.permute(2, 0, 1)  # x: HW, B, D
+        x, p = self.attn(x, x, x)  # x: HW, B, D
+        x = x.transpose(0, 1)  # x: B, HW, D
+        x = torch.mean(x, dim=1)  # x: B, D
         return self.classifier(x)

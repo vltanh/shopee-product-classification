f46117f65e2a5b1f937e101f87d8fc9898991103
vltanh <vltheanh030698@gmail.com>
2020-06-25 22:08:34 +0700

    fix aspp

--- models/classifier.py
+++ models/classifier.py
@@ -4,7 +4,7 @@ import torch.nn.functional as F
 from torchvision import models
 
 from utils import getter
-from .modules.aspp import ASPP_Bottleneck
+from .modules.aspp import ASPP
 
 
 class BaselineWithLinear(nn.Module):
@@ -26,16 +26,20 @@ class BaselineWithLinear(nn.Module):
 
 
 class BaselineWithASPP(nn.Module):
-    def __init__(self, extractor_cfg, hidden_dim, nclasses):
+    def __init__(self, extractor_cfg, aspp_hidden_dim, aspp_output_dim, nclasses):
         super().__init__()
         self.nclasses = nclasses
         self.extractor = getter.get_instance(extractor_cfg)
-        self.hidden_dim = hidden_dim
-        self.aspp = ASPP_Bottleneck(self.hidden_dim)
-        self.classifier = nn.Linear(self.hidden_dim, self.nclasses)
+        self.feature_dim = self.extractor.feature_dim
+        self.aspp_hidden_dim = aspp_hidden_dim
+        self.aspp_output_dim = aspp_output_dim
+        self.aspp = ASPP(self.feature_dim,
+                         self.aspp_hidden_dim,
+                         self.aspp_output_dim)
+        self.classifier = nn.Linear(self.aspp_output_dim, self.nclasses)
 
     def forward(self, x):
         x = self.extractor.get_feature_map(x)
         x = self.aspp(x)
-        x = F.adaptive_avg_pool2d(x, (1, 1)).view(-1, self.hidden_dim)
+        x = F.adaptive_avg_pool2d(x, (1, 1)).view(-1, self.aspp_output_dim)
         return self.classifier(x)
